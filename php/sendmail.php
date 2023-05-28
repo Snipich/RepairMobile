06270d107668eea9fbca40b55a21b789c1b13d5e
Snipich <123145923+Snipich@users.noreply.github.com>
2023-05-28 21:15:59 +0300

    Добавил почту

--- php/sendmail.php
+++ php/sendmail.php
@@ -8,7 +8,7 @@
     $subject = "=?utf-8?B?".base64_encode("Ремонт телефона")."?=";
     $headers ="From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
-    $success = mail("EMAIL", $subject,"Имя: ".$name.
+    $success = mail("admin@rostov-techservis.ru", $subject,"Имя: ".$name.
     "<br>Номер телефона: ".$phone."\n". "<br>Сообщение: ".$message, $headers);
 
     header("Location: ../index.html");
